1123084e78dae0e55008c70e74656bb9ac01216c
Niladri <niladrisengit12@gmail.com>
2023-07-28 19:18:24 +0530

    react-query-03-CUSTOM HOOK IN hooks folder

--- react_query_02/src/main.jsx
+++ react_query_02/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {QueryClientProvider,QueryClient} from 'react-query'
+import {ReactQueryDevtools} from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
   </QueryClientProvider>,
 )
--- react_query_02/src/pages/RQSuperheroes.jsx
+++ react_query_02/src/pages/RQSuperheroes.jsx
@@ -1,24 +1,27 @@
-import axios from 'axios'
-import React from 'react'
-import { useQuery } from 'react-query'
-
-const fetchSuperheroes = async()=> await axios.get(`http://localhost:4000/superheroes`)
+import { useSuperheroesData } from "../hooks/useSuperheroesData";
 
 const RQSuperheroes = () => {
-    const results = useQuery("super_hero_unique_key",fetchSuperheroes)
-    console.log(results)
-    const {isLoading,data,isError,error} = results;
-    
-    if (isLoading) {
-        return <h2>Loading...</h2>;
-      }
-    
-      return (
-        <div className="p-4">
-          <h2>Super Heroes Page</h2>
-          {data?.data?.map(hero =>  <div key={hero.name}>{hero.name}</div> )}
-        </div>
-      );
-}
 
-export default RQSuperheroes
+  const results = useSuperheroesData()
+  
+
+  const { isLoading, data, isError, error, refetch } = results;
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className="p-4">
+      <h2>Super Heroes Page</h2>
+      <button className="p-2 bg-red-600 text-white" onClick={refetch}>
+        Fetch Superheroes
+      </button>
+      {data?.data?.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default RQSuperheroes;
